57bb7bec867119d1dc82634dd7c4179b8eca5fc2
Ghassan <ghassanelgendyy@gmail.com>
2026-06-27 01:37:58 +0300

    chore: add build, dependency, typescript, and docker configurations

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 4200,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/images': {
+        target: 'http://127.0.0.1:3000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
+});
